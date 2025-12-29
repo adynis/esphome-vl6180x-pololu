1f4f8efae5e40a2770774b69ae71d34f8dbf698c
adynis <adynis@users.noreply.github.com>
2025-12-29 03:43:06 +0200

    Update sensor.py to support offset var

--- components/vl6180x_pololu/sensor.py
+++ components/vl6180x_pololu/sensor.py
@@ -12,12 +12,13 @@ VL6180XPololuHub = vl6180x_pololu_ns.class_("VL6180XPololuHub", cg.Component)
 VL6180XDistanceSensor = vl6180x_pololu_ns.class_("VL6180XDistanceSensor", cg.PollingComponent, sensor.Sensor)
 VL6180XALSSensor = vl6180x_pololu_ns.class_("VL6180XALSSensor", cg.PollingComponent, sensor.Sensor)
 
-# Configuration keys for the YAML schema
+# Configuration keys
 CONF_DISTANCE = "distance"
 CONF_DISTANCE_ERROR = "distance_error_code"
 CONF_AMBIENT_LIGHT = "ambient_light"
 CONF_ALS_ERROR = "ambient_light_error_code"
 CONF_GAIN = "gain"
+CONF_OFFSET = "offset"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(VL6180XPololuHub),
@@ -25,17 +26,20 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required("scl_pin"): cv.int_,
     cv.Optional(CONF_ADDRESS, default=0x29): cv.i2c_address,
     
-    # Range Sensor configuration
+    # Distance Sensor Configuration
     cv.Optional(CONF_DISTANCE): sensor.sensor_schema(
         VL6180XDistanceSensor, unit_of_measurement=UNIT_MILLIMETER, icon=ICON_RULER, 
         accuracy_decimals=0, device_class=DEVICE_CLASS_DISTANCE, state_class=STATE_CLASS_MEASUREMENT
-    ).extend(cv.polling_component_schema("100ms")),
+    ).extend(cv.polling_component_schema("100ms")).extend({
+        # Hardware offset to compensate for sensor/housing calibration issues
+        cv.Optional(CONF_OFFSET, default=0): cv.int_, 
+    }),
     
     cv.Optional(CONF_DISTANCE_ERROR): sensor.sensor_schema(
         unit_of_measurement="", icon="mdi:alert-circle-outline", accuracy_decimals=0
     ),
     
-    # Ambient Light Sensor configuration with adjustable Gain
+    # Ambient Light Sensor configuration
     cv.Optional(CONF_AMBIENT_LIGHT): sensor.sensor_schema(
         VL6180XALSSensor, unit_of_measurement=UNIT_LUX, icon="mdi:brightness-5", 
         accuracy_decimals=1, device_class=DEVICE_CLASS_ILLUMINANCE, state_class=STATE_CLASS_MEASUREMENT
@@ -49,29 +53,28 @@ CONFIG_SCHEMA = cv.Schema({
 })
 
 async def to_code(config):
-    # Instantiate the Hub component
+    # Setup Hub and I2C pins
     hub = cg.new_Pvariable(config[cv.GenerateID()])
     await cg.register_component(hub, config)
-    
-    # Set base I2C parameters
     cg.add(hub.set_sda_pin(config["sda_pin"]))
     cg.add(hub.set_scl_pin(config["scl_pin"]))
     cg.add(hub.set_address(config[CONF_ADDRESS]))
 
-    # Link error sensors if defined in YAML
+    # Link status/error sensors
     if CONF_DISTANCE_ERROR in config:
         cg.add(hub.set_distance_error_sensor(await sensor.new_sensor(config[CONF_DISTANCE_ERROR])))
-
     if CONF_ALS_ERROR in config:
         cg.add(hub.set_als_error_sensor(await sensor.new_sensor(config[CONF_ALS_ERROR])))
 
-    # Initialize Distance sensor
+    # Initialize Distance sensor and set hardware offset
     if CONF_DISTANCE in config:
-        var = await sensor.new_sensor(config[CONF_DISTANCE])
-        await cg.register_component(var, config[CONF_DISTANCE])
+        conf = config[CONF_DISTANCE]
+        var = await sensor.new_sensor(conf)
+        await cg.register_component(var, conf)
         cg.add(var.set_hub(hub))
+        cg.add(var.set_offset(conf[CONF_OFFSET]))
 
-    # Initialize ALS sensor and pass the Gain value
+    # Initialize ALS sensor and set Gain
     if CONF_AMBIENT_LIGHT in config:
         conf = config[CONF_AMBIENT_LIGHT]
         var = await sensor.new_sensor(conf)
